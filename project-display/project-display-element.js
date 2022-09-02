0e75398337792971525cc94a0402f4f4331361e5
Samuel Sarette <samuelsarette@linux.com>
2022-09-01 20:36:31 -0400

    Added photos and filled out project-display element properties

--- project-display/project-display-element.js
+++ project-display/project-display-element.js
@@ -30,14 +30,72 @@ export default class ProjectDisplayElement extends HTMLElement {
             clone.querySelector("ul.tags").appendChild(listItemEl);
         });
 
-        // Set Background of thumbnail
+        // Set Thumbnail
+        let thumbStyle = clone.querySelector(".thumbnail").style;
         if (this.hasAttribute("thumbnail")) {
-            clone.querySelector(".thumbnail").style.backgroundImage = `url(${this.getAttribute("thumbnail")})`;
+            thumbStyle.backgroundImage = `url(${this.getAttribute("thumbnail")})`;
+        }
+        if (this.hasAttribute("thumbnail-size")) {
+            thumbStyle.backgroundSize = this.getAttribute("thumbnail-size");
+        }
+        if (this.hasAttribute("thumbnail-repeat")) {
+            thumbStyle.backgroundRepeat = this.getAttribute("thumbnail-repeat");
         }
 
-        // Set Title
-        if (this.hasAttribute("title")) {
-            clone.querySelector(".title").textContent = this.getAttribute("title");
+        // Set Links
+        let linksEl = clone.querySelector(".links");
+        if (this.hasAttribute("link-main")) {
+            linksEl.querySelector(".main").href = this.getAttribute("link-main");
+            linksEl.querySelector(".main").classList.remove("hidden");
+            linksEl.classList.remove("hidden");
+        }
+        if (this.hasAttribute("link-repo")) {
+            linksEl.querySelector(".repo").href = this.getAttribute("link-repo");
+            linksEl.querySelector(".repo").classList.remove("hidden");
+            linksEl.classList.remove("hidden");
+        }
+        if (this.hasAttribute("link-announcement")) {
+            linksEl.querySelector(".announcement").href = this.getAttribute("link-announcement");
+            linksEl.querySelector(".announcement").classList.remove("hidden");
+            linksEl.classList.remove("hidden");
+        }
+
+        // Set Dates
+        if (this.hasAttribute("released")) {
+            let dateTimeEl = clone.querySelector(".published time");
+            let dateTimeVal = this.getAttribute("released");
+            dateTimeEl.setAttribute('datetime', dateTimeVal);
+            dateTimeEl.textContent = dateTimeVal;
+            clone.querySelector(".published").classList.remove("hidden");
+        }
+        if (this.hasAttribute("first")) {
+            let dateTimeEl = clone.querySelector(".timeframe time.first");
+            let dateTimeVal = this.getAttribute("first");
+            dateTimeEl.setAttribute('datetime', dateTimeVal);
+            dateTimeEl.textContent = dateTimeVal;
+            dateTimeEl.classList.remove("hidden");
+            clone.querySelector(".timeframe").classList.remove("hidden");
+        }
+        if (this.hasAttribute("last")) {
+            let dateTimeEl = clone.querySelector(".timeframe time.last");
+            let dateTimeVal = this.getAttribute("last");
+            dateTimeEl.setAttribute('datetime', dateTimeVal);
+            dateTimeEl.textContent = dateTimeVal;
+            dateTimeEl.classList.remove("hidden");
+            clone.querySelector(".timeframe").classList.remove("hidden");
+        }
+
+        // Set For (Company, Organization)
+        if (this.hasAttribute("for")) {
+            clone.querySelector(".for").textContent = this.getAttribute("for");
+            clone.querySelector(".for").classList.remove("hidden");
+        }
+        
+        // Set Name (Title)
+        if (this.hasAttribute("name")) {
+            clone.querySelector(".name").textContent = this.getAttribute("name");
+        } else {
+            console.warn("project-display element does not have a name!", this);
         }
         
         
