ef6a50aedf7c1c2e6785e0829078cacdbd9de655
Samuel Sarette <samuelsarette@linux.com>
2022-09-04 08:29:27 -0400

    fix issue with same-page determination when no referrer exists

--- animated-dialog/animated-dialog-element.js
+++ animated-dialog/animated-dialog-element.js
@@ -1,30 +1,30 @@
 
 export default class AnimatedDialogElement extends HTMLDialogElement {
 
-	closing = false;
+    closing = false;
 
-	constructor(){
-		super();
-	}
+    constructor(){
+        super();
+    }
 
-	close() {
-		if (this.closing) return;
-		this.closing = true;
+    close() {
+        if (this.closing) return;
+        this.closing = true;
 
-		this.toggleAttribute("dismissing", true);
+        this.toggleAttribute("dismissing", true);
 
-		if (window.matchMedia("(prefers-reduced-motion: reduce)").matches)
-			this.#closeFinish();
-		else
-			this.addEventListener("animationend", this.#closeFinish, {once: true});
+        if (window.matchMedia("(prefers-reduced-motion: reduce)").matches)
+            this.#closeFinish();
+        else
+            this.addEventListener("animationend", this.#closeFinish, {once: true});
         
-	}
+    }
 
-	#closeFinish() {
-		super.close();
-		this.toggleAttribute("dismissing", false);
-		this.closing = false;
-	}
+    #closeFinish() {
+        super.close();
+        this.toggleAttribute("dismissing", false);
+        this.closing = false;
+    }
 }
 
 // Register element
--- controllers/index.js
+++ controllers/index.js
@@ -3,8 +3,8 @@ import "../page-fade/page-fade.js";
 
 // On Loaded
 document.addEventListener("DOMContentLoaded", () => {
-	// Setup Modal
-	var testDialog = document.getElementById("about-dialog");
-	document.getElementById("open-about-dialog").addEventListener("click", () => testDialog.showModal());
-	testDialog.querySelector("button").addEventListener("click", () => testDialog.close());
+    // Setup Modal
+    var testDialog = document.getElementById("about-dialog");
+    document.getElementById("open-about-dialog").addEventListener("click", () => testDialog.showModal());
+    testDialog.querySelector("button").addEventListener("click", () => testDialog.close());
 });
